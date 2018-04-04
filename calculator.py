53e07d5eccdaa55369dfbb7a0eac567ecd5a7b90
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 15:45:26 -0700

    added multiplication to calc

--- calculator.py
+++ calculator.py
@@ -27,6 +27,8 @@ def calculator():
                 print add(int(tokens[1]), int(tokens[2]))
             elif tokens[0] == "-":
                 print subtract(int(tokens[1]), int(tokens[2]))
+            elif tokens[0] == "*":
+                print multiply(int(tokens[1]), int(tokens[2]))
 
 
 calculator()         
