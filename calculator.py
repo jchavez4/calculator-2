91587ff7075feabc48013ee8d2514b502842f5be
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 15:50:26 -0700

    added division functionality.

--- calculator.py
+++ calculator.py
@@ -24,11 +24,13 @@ def calculator():
             return
         else:
             if tokens[0] == "+":
-                print add(int(tokens[1]), int(tokens[2]))
+                print add(float(tokens[1]), float(tokens[2]))
             elif tokens[0] == "-":
-                print subtract(int(tokens[1]), int(tokens[2]))
+                print subtract(float(tokens[1]), float(tokens[2]))
             elif tokens[0] == "*":
-                print multiply(int(tokens[1]), int(tokens[2]))
+                print multiply(float(tokens[1]), float(tokens[2]))
+            elif tokens[0] == "/":
+                print divide(float(tokens[1]), float(tokens[2]))
 
 
 calculator()         
