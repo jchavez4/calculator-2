ecb32ccdebc180559d91d06bc959f5851b77a119
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-04 16:16:26 -0700

    arithmetic that takes a list as input

--- arithmetic.py
+++ arithmetic.py
@@ -1,60 +1,78 @@
-"""Math functions for calculator."""
+# """Math functions for calculator."""
 
 
-def add(num1, num2):
-    """Return the sum of the two inputs."""
-    return num1 + num2
+# def add(num1, num2):
+#     """Return the sum of the two inputs."""
+#     return num1 + num2
 
 
-def subtract(num1, num2):
-    """Return the second number subtracted from the first."""
+# def subtract(num1, num2):
+#     """Return the second number subtracted from the first."""
 
-    return num1 - num2
-
-
-def multiply(num1, num2):
-    """Multiply the two inputs together."""
-
-    return num1 * num2
+#     return num1 - num2
 
 
-def divide(num1, num2):
-    """Divide the first input by the second and return the result."""
+# def multiply(num1, num2):
+#     """Multiply the two inputs together."""
 
-    return num1/num2
-
-
-def square(num1):
-    """Return the square of the input."""
-
-    return num1 ** 2
+#     return num1 * num2
 
 
-def cube(num1):
-    """Return the cube of the input."""
+# def divide(num1, num2):
+#     """Divide the first input by the second and return the result."""
 
-    return num1 ** 3
-
-
-def power(num1, num2):
-    """Raise num1 to the power of num2 and return the value."""
-
-    return num1 ** num2
+#     return num1/num2
 
 
-def mod(num1, num2):
-    """Return the remainder of num1 / num2."""
+# def square(num1):
+#     """Return the square of the input."""
 
-    return num1 % num2
-
-
-def add_mult(num1, num2, num3):
-    """Return the result of adding num1 and num2 multiplied by num3."""
-
-    return (num1 + num2) * num3
+#     return num1 ** 2
 
 
-def add_cubes(num1, num2):
-    """Return the result of adding the cubes of num1 and num2."""
+# def cube(num1):
+#     """Return the cube of the input."""
 
-    return cube(num1) + cube(num2)
+#     return num1 ** 3
+
+
+# def power(num1, num2):
+#     """Raise num1 to the power of num2 and return the value."""
+
+#     return num1 ** num2
+
+
+# def mod(num1, num2):
+#     """Return the remainder of num1 / num2."""
+
+#     return num1 % num2
+
+
+# def add_mult(num1, num2, num3):
+#     """Return the result of adding num1 and num2 multiplied by num3."""
+
+#     return (num1 + num2) * num3
+
+
+# def add_cubes(num1, num2):
+#     """Return the result of adding the cubes of num1 and num2."""
+
+#     return cube(num1) + cube(num2)
+
+def add(num_list):
+    """Returns sum of numbers in the list."""
+    sum = 0
+    for num in num_list:
+        sum += num
+    return sum
+
+
+def subtract(num_list):
+    """Returns the difference of numbers in the list."""
+    difference = num_list[0]
+    for num in range(1, len(num_list)):
+        difference -= num_list[num]
+    return difference
+
+print add([5,2,3])
+print subtract([5,10,10])
